44c5618debc4a42d5d6786145430d8b318baa761
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-08-31 15:15:15 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.05";
+       # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+       # url = "github:nix-community/nixvim/nixos-24.05";
+
+      inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, ... }: {
+    nixosConfigurations.ilma4-vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      #specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+	nixvim.nixosModules.nixvim
+	# nixvim.homeManagerModules.nixvim
+	home-manager.nixosModules.default
+      ];
+    };
+  };
+}
