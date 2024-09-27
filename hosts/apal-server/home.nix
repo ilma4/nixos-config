74cb0fff194dbfadef9a7d6a5a7a7fac8458c8e6
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-09-27 18:37:08 +0200

    home-manager config split

--- hosts/apal-server/home.nix
+++ hosts/apal-server/home.nix
@@ -2,14 +2,13 @@
 
 {
   imports = [
-     #<nixvim>.homeManagerModules.nixvim
+    ./../../common/home/base.nix
+    ./../../common/home/work.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "malakhov";
   home.homeDirectory = "/home/malakhov";
-
-  nixpkgs.config.allowUnfree = true;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -19,112 +18,4 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.05" ; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    screen
-    # nerdfonts
-    powerline-fonts
-    curl
-    wget
-
-    unrar
-    unzip
-    zip
-    zstd
-    xz
-    gzip
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  fonts.fontconfig.enable = true;
-
-  programs.git = { 
-    enable = true;
-    userName  = "Ilia Malakhov";
-    userEmail = "ilya.malakhov4@gmail.com";
-  };
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "vi-mode" "extract " ];
-      theme = "apple";
-    };
-  };
-
-  programs.atuin = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  programs.nixvim = {
-    enable = true;
-    clipboard.providers.wl-copy.enable = true;
-    clipboard.register = "unnamedplus";
-    extraPlugins = [
-      pkgs.vimPlugins."vim-suda"
-    ];
-  };
-
-  programs.ripgrep.enable = true;
-  programs.fd.enable = true;
-  programs.bat.enable = true;
-  programs.htop.enable = true;
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/ilma4/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
