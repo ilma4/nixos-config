e408baa66ce9c0ec69cc57d6b8d1c38ea2cf1361
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-09-28 10:44:16 +0200

    nixvim

--- common/home/nixvim.nix
+++ common/home/nixvim.nix
@@ -9,6 +9,11 @@
     };
 
     plugins.treesitter.enable = true;
+    plugins.cmp.enable = true;
+    plugins.cmp.autoEnableSources = true;
+    plugins.lsp.enable = true;
+    plugins.auto-save.enable = true;
+
     extraPlugins = [
       pkgs.vimPlugins."vim-suda"
     ];
