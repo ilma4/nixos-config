487c1f3cd250b74a4c9613c433a32cd2fd88b0dd
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-10-04 12:33:51 +0200

    disable darkman

--- hosts/main/home.nix
+++ hosts/main/home.nix
@@ -14,37 +14,12 @@
   targets.genericLinux.enable = true;
   xdg.mime.enable = true;
 
-  services.darkman.settings = {
-    # Berlin 
-    lat = 52.5;
-    lng = 13.4;
-
-    usegeoclue = true;
-  };
-
   services.easyeffects.enable = true;
 
-  services.darkman.darkModeScripts = {
-    gtk-theme = ''
-      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
-      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-dark'"
-    '';
-  };
-
-  services.darkman.lightModeScripts = {
-    gtk-theme = ''
-      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
-      ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/gtk-theme "'Yaru-light'"
-    '';
-  };
 
   #xdg.enable = true;
   #xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.darkman pkgs.xdg-desktop-portal-wlr ];
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-wlr ];
   xdg.portal.config = {
-    common = {
-      "org.freedesktop.impl.portal.Settings" = [ "darkman" ];
-      "org.freedesktop.portal.Settings" = [ "darkman" ];
-    };
   };
 }
