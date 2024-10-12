6b939a07c6edaae916ee401dfa983075abc29258
Ilia Malakhov <90174552+ilma4@users.noreply.github.com>
2024-10-12 07:55:55 +0200

    top-command optimizations

--- hosts/main/top-commands.nix
+++ hosts/main/top-commands.nix
@@ -4,7 +4,7 @@ let cfg = config.top-commands; in
   options.top-commands = {
     commands = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
-      example = { hello-world = "echo 'Hello World!'"; };
+      example = { hello-world = "echo 'Hello World!'"; "a" = "echo 'A'"; };
       description = "Named favourite commands";
     };
     tofi-command = lib.mkOption {
@@ -16,9 +16,8 @@ let cfg = config.top-commands; in
   
 
   config = let 
-    # commandsAsStr = with builtins; concatStringsSep "\n" (attrValues (mapAttrs (key: value: "${key} ${value}") cfg.commands ));
-    commandsAsStr = lib.attrsets.foldlAttrs 
-      (acc: key: value: "${acc}\n${key} ${value}")
+    commandsAsMap = lib.attrsets.foldlAttrs 
+      (acc: key: value: "${acc}\n(\"${key}\", \"${value}\"),")
       ""
       cfg.commands
      ; 
@@ -27,44 +26,25 @@ let cfg = config.top-commands; in
     ;
   in let 
     launch-favorite = (pkgs.writers.writeRustBin "launch-favorite" { rustcArgs = [ "-C" "opt-level=3" ]; } /*rust*/''
-      use std::env;
-      use std::os::unix::process::CommandExt;
-      use std::process::{Command, Stdio};
+    use std::env;
+    use std::os::unix::process::CommandExt;
+    use std::process::{Command, Stdio};
+    use std::collections::HashMap;
 
-      const FAVORITE_COMMANDS: &str = r#"
-${commandsAsStr}
-      "#;
-
-
-      fn get_command(command_name: &str) -> &'static str {
-          FAVORITE_COMMANDS
-              .lines()
-              .filter(|line| !line.trim().is_empty())
-              .find_map(|line| {
-                  let mut parts = line.splitn(2, ' ');
-                  let name = parts.next().unwrap();
-                  if name != command_name {
-                      None
-                  } else {
-                      let command = parts.next().unwrap();
-                      Some(command)
-                  }
-              })
-              .unwrap()
-      }
-
-
-      fn main() {
-          let command_name = env::args().nth(1).unwrap();
-          let mut command = get_command(command_name.as_str()).split_whitespace();
-          let err = Command::new(command.next().unwrap())
-              .args(command)
-              .stdout(Stdio::inherit())
-              .stderr(Stdio::inherit())
-              .stdin(Stdio::inherit())
-              .exec();
-          eprintln!("Command {command_name} failed with error {err}");
-      }
+    fn main() {
+        let command_name = env::args().nth(1).unwrap();
+        let name_to_command = HashMap::from([
+            ${commandsAsMap}
+        ]);
+        let mut command = name_to_command[command_name.as_str()].split_whitespace();
+        let err = Command::new(command.next().unwrap())
+            .args(command)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .stdin(Stdio::inherit())
+            .exec();
+        eprintln!("Command {command_name} failed with error {err}");
+    }
     '');
 
     select-favorite = (pkgs.writeShellScriptBin "select-favorite" ''
@@ -73,11 +53,12 @@ ${commandsAsStr}
     exec-favorite = (pkgs.writeShellScriptBin "exec-favorite" ''
       ${select-favorite}/bin/select-favorite | xargs -I{} ${launch-favorite}/bin/launch-favorite {}
     '');
+    
+    sway-modifier = config.wayland.windowManager.sway.config.modifier;
     in {
     home.packages = [launch-favorite exec-favorite select-favorite];
-
     wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
-      "${config.wayland.windowManager.sway.config.modifier}+y" = "exec ${exec-favorite}/bin/exec-favorite";
+      "${sway-modifier}+y" = "exec ${exec-favorite}/bin/exec-favorite";
     };
   };
 }
