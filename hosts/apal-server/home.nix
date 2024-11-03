e5aea95988a6a3006cd2f16f75f081e34a2520df
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-11-03 19:02:44 +0100

    fix for apal-server

--- hosts/apal-server/home.nix
+++ hosts/apal-server/home.nix
@@ -12,4 +12,8 @@
   home.username = "malakhov";
   home.homeDirectory = "/home/malakhov";
   flake-location = "github:ilma4/nixos-config";
+
+  programs.zsh.profileExtra = ''
+    if [ -e /home/malakhov/.nix-profile/etc/profile.d/nix.sh ]; then . /home/malakhov/.nix-profile/etc/profile.d/nix.sh; fi # added by Nix installer
+  '';
 }
