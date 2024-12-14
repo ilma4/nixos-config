f9be3b5a6b8f1ee1ccdf98eca30ca19ce649ce74
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-12-14 10:21:53 +0100

    syncthing for ilma4-bkp

--- hosts/bkp/configuration.nix
+++ hosts/bkp/configuration.nix
@@ -81,6 +81,10 @@
     pulse.enable = true;
   };
 
+  services.syncthing = {
+    enable = true;
+  };
+
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
