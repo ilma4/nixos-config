746330406f9ffd440e781a555a3d9b1585d8471c
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-01-23 22:23:18 +0100

    avahi is back!

--- hosts/bkp/configuration.nix
+++ hosts/bkp/configuration.nix
@@ -39,12 +39,6 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-  networking.networkmanager.dns = "systemd-resolved";
-
-  services.resolved = {
-    enable = true;
-    extraConfig = "MulticastDNS=yes";
-  };
 
   # Set your time zone.
   time.timeZone = "Europe/Amsterdam";
--- hosts/bkp/server.nix
+++ hosts/bkp/server.nix
@@ -39,6 +39,19 @@
     };
   };
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    ipv6 = true;
+    publish = {
+      enable = true;
+      domain = true;
+      addresses = true;
+    };
+    reflector = true;
+  };
+
 /*
   services.dnsmasq = {
     enable = true;
