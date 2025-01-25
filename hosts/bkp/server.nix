09a5ffc7870086a5d3b2229e54884e238021609b
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-01-25 14:28:06 +0100

    nginx somewhat works

--- hosts/bkp/server.nix
+++ hosts/bkp/server.nix
@@ -18,6 +18,16 @@
   users.groups.homeassistant = {};
   users.groups.homer = {};
 
+
+  virtualisation.podman = {
+    enable = true;
+    autoPrune.enable = true;
+    defaultNetwork.settings = {
+      # Required for container networking to be able to use names.
+      dns_enabled = true;
+    };
+  };
+
   virtualisation.oci-containers.containers = {
     homeassistant = {
       image = "ghcr.io/home-assistant/home-assistant:stable";
@@ -32,10 +42,21 @@
 
     homer = {
       image = "b4bz/homer:latest";
-      ports = ["8080:8080"];
+      ports = ["8080"];
       volumes = ["${dotfiles}/homer:/www/assets:ro" "/etc/passwd:/etc/passwd:ro" "/etc/group:/etc/group:ro"];
       autoStart = true;
       user = "homer:homer";
+      hostname = "homer";
+      extraOptions = ["--network=host"];
+      # extraOptions = ["--network=nginx"];
+    };
+
+    nginx = {
+      image = "nginx:stable";
+      ports = ["80:80" "443:443"];
+      volumes = ["${dotfiles}/nginx:/etc/nginx:ro" /*"/etc/letsencrypt:/etc/letsencrypt:ro"*/ ];
+      autoStart = true;
+      extraOptions = ["--network=host"];
     };
   };
 
@@ -56,5 +77,9 @@
     8123 # home-assistant
     8080 # homer
     2283 # immich
+
+    # nginx
+    80
+    443
   ];
 }
