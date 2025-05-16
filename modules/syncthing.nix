a223a33a31312d0015f31f25e29f2ce2454cf958
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-05-16 14:30:39 +0200

    syncthing healthcheck

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -12,8 +12,14 @@
         "/etc/localtime:/etc/localtime:ro"
       ];
       hostname = "ilma4-bkp-syncthing";
-      extraOptions = ["--network=host"]; # so UPnP mapping will work
-      # TODO: healthcheck
+      extraOptions = [
+        "--network=host" # so UPnP mapping will work
+        "--health-cmd=curl -fkLsS -m 2 127.0.0.1:8384/rest/noauth/health | grep -o --color=never OK || exit 1 "
+        "--health-interval=1m0s"
+        "--health-retries=3"
+        "--health-start-period=20s"
+        "--health-timeout=10s"
+      ];
     };
   };
   networking.firewall.allowedTCPPorts = [
