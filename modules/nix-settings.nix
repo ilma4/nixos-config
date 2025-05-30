fc58c43bf3c32f34099aec0ee64fdd1883c03165
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-05-30 22:06:00 +0200

    fix

--- modules/nix-settings.nix
+++ modules/nix-settings.nix
@@ -5,7 +5,7 @@
     optimise.automatic = true;
     settings = {
       # allowed-users = [ "ilma4" ];
-      sandbox = true; # enabled on linux by default FIXME: broken on nix-darwin 24.11, TODO: check on nix-darwin 25.05, should be fixed
+      # sandbox = true; # enabled on linux by default FIXME: broken on nix-darwin 24.11, TODO: check on nix-darwin 25.05, should be fixed
 
       auto-optimise-store = false; # DO NOT enable. Use optimize.automatic instead
       experimental-features = "nix-command flakes"; # Necessary for using flakes on this system.
