b081e126108529f8859f811f1cdacb7e89d4a979
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-07-01 11:09:09 +0200

    Auto-attach to tmux session when connecting to ilma4-nas via ssh

--- hosts/nas/home.nix
+++ hosts/nas/home.nix
@@ -13,6 +13,19 @@
   home.username = "ilma4";
   flake-location = "${config.home.homeDirectory}/nixos";
 
+  # Initialize tmux session on SSH connection
+  programs.zsh.initContent = ''
+    if [ -n "$SSH_CONNECTION" ] && [ -z "$TMUX" ]; then
+      tmux attach-session -t default || tmux new-session -s default
+    fi
+  '';
+
+  programs.bash.initExtra = ''
+    if [ -n "$SSH_CONNECTION" ] && [ -z "$TMUX" ]; then
+      tmux attach-session -t default || tmux new-session -s default
+    fi
+  '';
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
