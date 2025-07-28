6cffbc81d5064d625fa7b6748624b53d9b5db63f
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-07-28 22:01:32 +0200

    `sops.nix` can be used as home-manager module

--- modules/sops.nix
+++ modules/sops.nix
@@ -16,7 +16,10 @@
     sops.defaultSopsFile = "${flake-location}/secrets/example.yaml";
     sops.defaultSopsFormat = "yaml";
     # sops.age.keyFile = "/home/ilma4/.config/sops/age/keys.txt";
-    sops.age.keyFile = "${config.users.users.ilma4.home}/.config/sops/age/keys.txt";
+    sops.age.keyFile =
+      if (builtins.hasAttr "users" config) # true if NixOS/Nix-darwin
+      then "${config.users.users.ilma4.home}/.config/sops/age/keys.txt"
+      else "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
     # map i4-encrypted-files
     sops.secrets = lib.pipe config.i4-encrypted-files [
