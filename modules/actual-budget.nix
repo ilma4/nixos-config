b1c1c2bb37275f8c3d9f72714aee92c116c5fadf
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-08-29 22:34:34 +0200

    actual-budget migrated to compose

--- modules/actual-budget.nix
+++ modules/actual-budget.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  flake-location,
+  config,
+  ...
+}: {
   users.users.actual-budget = {
     isSystemUser = true;
     uid = 800;
@@ -6,25 +10,14 @@
   };
   users.groups.actual-budget.gid = config.users.users.actual-budget.uid;
 
-  virtualisation.oci-containers.containers = {
-    actual-budget = {
-      image = "docker.io/actualbudget/actual-server:25.8.0-alpine"; # TODO: latest-alpine";
-      volumes = [
-        "/srv/actual-budget:/data:rw"
-      ];
-      ports = [
-        "5006:5006/tcp"
-      ];
-      user = "${toString config.users.users.actual-budget.uid}:${toString config.users.groups.actual-budget.gid}";
-      extraOptions = [
-        "--health-cmd=node src/scripts/health-check.js"
-        "--health-interval=1m0s"
-        "--health-retries=3"
-        "--health-start-period=20s"
-        "--health-timeout=10s"
-      ];
-    };
+  dockerCompose.actual-budget.composeFile = "${flake-location}/compose/actual-budget.yml";
+  dockerCompose.actual-budget.environment = {
+    UID_GID = "${toString config.users.users.actual-budget.uid}:${toString config.users.groups.actual-budget.gid}";
   };
+
+  systemd.tmpfiles.rules = [
+    "d /srv/actual-budget 0755 actual-budget actual-budget -"
+  ];
 
   networking.firewall.allowedTCPPorts = [
     5006
