ee10d13b8518dd05a846b4703a610a250182b35a
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-08-30 23:19:21 +0200

    home-assistant

--- modules/nginx-reverse-proxy.nix
+++ modules/nginx-reverse-proxy.nix
@@ -59,21 +59,35 @@
     }))
     attrValues
   ];
-  nginxServerConfs = concatStringsSep "\n" (map (c: ''
+  nginxServerConfs =
+    (concatStringsSep "\n" (map (c: ''
+        server {
+          listen 80;
+          server_name ${c.name}.ilma4.local;
+          location / {
+            proxy_pass http://${c.name}:${toString c.port};
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          }
+        }
+      '')
+      containers))
+    + ''
       server {
         listen 80;
-        server_name ${c.name}.ilma4.local;
+        server_name home-assistant.ilma4.local;
         location / {
-          proxy_pass http://${c.name}:${toString c.port};
-          proxy_set_header Host $host;
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
+            proxy_pass http://127.0.0.1:8123;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
         }
       }
-    '')
-    containers);
-  nginxConf = pkgs.writeText "reverse_proxy.conf" nginxServerConfs;
+    '';
+  nginxConf = pkgs.writeText "reverse_proxy.conf" (lib.trace nginxServerConfs nginxServerConfs);
   composeYaml = ''
     services:
       nginx-reverse-proxy:
