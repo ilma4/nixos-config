59a93373359185c889784b5e9b7d94ae40a4bc00
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-03 21:22:18 +0200

    [nginx] nginx config tweaks

--- modules/nginx-reverse-proxy.nix
+++ modules/nginx-reverse-proxy.nix
@@ -127,14 +127,18 @@
 
         location / {
           proxy_pass ${c.upstream};
-          proxy_http_version 1.1;
 
+          # These configuration options are required for WebSockets to work
+          proxy_http_version 1.1;
          	proxy_set_header Upgrade $http_upgrade;
          	proxy_set_header Connection "upgrade";
-          proxy_set_header Host $host;
+
+          proxy_redirect off;
+          proxy_set_header Host $host:$server_port;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto $scheme;
+          add_header Referrer-Policy "strict-origin-when-cross-origin";
         }
       }
     '')
