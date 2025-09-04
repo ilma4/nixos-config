b237b315cf5c87d776824ae24ff1a2fd0b235129
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-04 20:44:02 +0200

    refactoring

--- hosts/anakin/top-commands.nix
+++ hosts/anakin/top-commands.nix
@@ -1,10 +1,18 @@
-inputs@{ config, lib, pkgs, ... }:
-let cfg = config.top-commands; in
-{
+inputs @ {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.top-commands;
+in {
   options.top-commands = {
     commands = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
-      example = { hello-world = "echo 'Hello World!'"; "a" = "echo 'A'"; };
+      example = {
+        hello-world = "echo 'Hello World!'";
+        "a" = "echo 'A'";
+      };
       description = "Named favourite commands";
     };
     tofi-command = lib.mkOption {
@@ -13,49 +21,54 @@ let cfg = config.top-commands; in
       description = "Command to run tofi";
     };
   };
-  
 
-  config = let 
-    commandsAsMap = lib.attrsets.foldlAttrs 
+  config = let
+    commandsAsMap =
+      lib.attrsets.foldlAttrs
       (acc: key: value: "${acc}\n(\"${key}\", \"${value}\"),")
       ""
-      cfg.commands
-     ; 
-    commandNames = lib.attrsets.foldlAttrs
-      (acc: key: _: "${acc}\n${key}") "" cfg.commands
-    ;
-  in let 
-    launch-favorite = (pkgs.writers.writeRustBin "launch-favorite" { rustcArgs = [ "-C" "opt-level=3" ]; } /*rust*/''
-    use std::env;
-    use std::os::unix::process::CommandExt;
-    use std::process::{Command, Stdio};
-    use std::collections::HashMap;
+      cfg.commands;
+    commandNames =
+      lib.attrsets.foldlAttrs
+      (acc: key: _: "${acc}\n${key}") ""
+      cfg.commands;
+  in let
+    launch-favorite =
+      pkgs.writers.writeRustBin "launch-favorite" {rustcArgs = ["-C" "opt-level=3"];}
+      /*
+      rust
+      */
+      ''
+        use std::env;
+        use std::os::unix::process::CommandExt;
+        use std::process::{Command, Stdio};
+        use std::collections::HashMap;
 
-    fn main() {
-        let command_name = env::args().nth(1).unwrap();
-        let name_to_command = HashMap::from([
-            ${commandsAsMap}
-        ]);
-        let mut command = name_to_command[command_name.as_str()].split_whitespace();
-        let err = Command::new(command.next().unwrap())
-            .args(command)
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .stdin(Stdio::inherit())
-            .exec();
-        eprintln!("Command {command_name} failed with error {err}");
-    }
-    '');
+        fn main() {
+            let command_name = env::args().nth(1).unwrap();
+            let name_to_command = HashMap::from([
+                ${commandsAsMap}
+            ]);
+            let mut command = name_to_command[command_name.as_str()].split_whitespace();
+            let err = Command::new(command.next().unwrap())
+                .args(command)
+                .stdout(Stdio::inherit())
+                .stderr(Stdio::inherit())
+                .stdin(Stdio::inherit())
+                .exec();
+            eprintln!("Command {command_name} failed with error {err}");
+        }
+      '';
 
-    select-favorite = (pkgs.writeShellScriptBin "select-favorite" ''
+    select-favorite = pkgs.writeShellScriptBin "select-favorite" ''
       printf "${commandNames}" | ${cfg.tofi-command}
-    '');
-    exec-favorite = (pkgs.writeShellScriptBin "exec-favorite" ''
+    '';
+    exec-favorite = pkgs.writeShellScriptBin "exec-favorite" ''
       ${select-favorite}/bin/select-favorite | xargs -I{} ${launch-favorite}/bin/launch-favorite {}
-    '');
-    
+    '';
+
     sway-modifier = config.wayland.windowManager.sway.config.modifier;
-    in {
+  in {
     home.packages = [launch-favorite exec-favorite select-favorite];
     wayland.windowManager.sway.config.keybindings = lib.mkOptionDefault {
       "${sway-modifier}+y" = "exec ${exec-favorite}/bin/exec-favorite";
