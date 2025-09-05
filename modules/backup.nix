eaa22579469c9272244087a9108887e80f71587a
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-05 23:14:28 +0200

    fix

--- modules/backup.nix
+++ modules/backup.nix
@@ -7,13 +7,13 @@
   config = {
     users.users.backup = {
       isSystemUser = true;
-      group = "actual-budget";
+      group = config.users.groups.backup.name;
     };
     users.groups.backup = {};
 
     systemd.tmpfiles.rules = let
-      backup-user = config.users.users.backup;
-      backup-group = config.users.groups.backup;
+      backup-user = config.users.users.backup.name;
+      backup-group = config.users.groups.backup.name;
     in [
       "d /var/restic 0750 ${backup-user} ${backup-group} -"
     ];
