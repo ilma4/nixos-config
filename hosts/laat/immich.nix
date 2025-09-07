e5ecfdc860a091fa6f2b8ea50b8bdbba8c82df99
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-07 12:32:05 +0200

    open port for immich

--- hosts/laat/immich.nix
+++ hosts/laat/immich.nix
@@ -13,6 +13,8 @@ in {
 
   systemd.services.immich.serviceConfig.WorkingDirectory = dir; # ./immich;
 
+  networking.firewall.allowedTCPPorts = [2283];
+
   systemd.tmpfiles.rules = [
     "d /srv/immich/library 700 root root -"
     "d /srv/immich/postgres 700 root root -"
