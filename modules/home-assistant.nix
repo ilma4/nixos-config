bb34208ffddda95e86d9f852ae36de7ae9c2cd13
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-12 23:44:06 +0200

    [home-assistant] inline compose.yml to .nix module

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -1,4 +1,6 @@
-{lib, ...}: {
+{pkgs, ...}: let
+  CONFIG_DIR = "/srv/homeassistant";
+in {
   users.users.homeassistant = {
     isSystemUser = true;
     uid = 990;
@@ -6,10 +8,22 @@
   };
   users.groups.homeassistant.gid = 986;
 
-  dockerCompose.home-assistant.composeFile = "${lib.flake-location}/compose/home-assistant.yml";
-  dockerCompose.home-assistant.environment = {
-    CONFIG_DIR = "/srv/homeassistant";
-  };
+  dockerCompose.home-assistant.composeFile = pkgs.writeText "docker-compose.yml" ''
+    name: home-assistant
+    services:
+      home-assistant:
+        container_name: home-assistant
+        image: "ghcr.io/home-assistant/home-assistant:stable"
+        volumes:
+          - ${CONFIG_DIR}:/config
+          - /etc/localtime:/etc/localtime:ro
+          - /run/dbus:/run/dbus:ro
+        labels:
+          - "local.ilma4.customResolve=10.20.0.1:8123" # reverse-proxy will use this IP address to resolve container instead of the container's hostname
+        network_mode: host
+        restart: unless-stopped
+        privileged: true
+  '';
 
   networking.firewall.allowedTCPPorts = [8123];
 
