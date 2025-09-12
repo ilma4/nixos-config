4c7639ad762880a5234eb93b810f7b82216c0f87
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-12 23:45:37 +0200

    [actual-budget] inline compose.yml to .nix module

--- modules/actual-budget.nix
+++ modules/actual-budget.nix
@@ -1,8 +1,10 @@
 {
   config,
-  lib,
+  pkgs,
   ...
-}: {
+}: let
+  UID_GID = "${toString config.users.users.actual-budget.uid}:${toString config.users.groups.actual-budget.gid}";
+in {
   users.users.actual-budget = {
     isSystemUser = true;
     uid = 800;
@@ -10,10 +12,35 @@
   };
   users.groups.actual-budget.gid = config.users.users.actual-budget.uid;
 
-  dockerCompose.actual-budget.composeFile = "${lib.flake-location}/compose/actual-budget.yml";
-  dockerCompose.actual-budget.environment = {
-    UID_GID = "${toString config.users.users.actual-budget.uid}:${toString config.users.groups.actual-budget.gid}";
-  };
+  dockerCompose.actual-budget.composeFile = pkgs.writeText "docker-compose.yml" ''
+    # Actual Budget - Docker Compose
+    # Expects an environment variable UID_GID (format: "<uid>:<gid>") to run the container as a specific user.
+    # Example: export UID_GID="800:800"
+
+    name: actual-budget
+    services:
+      actual-budget:
+        container_name: actual-budget
+        image: "ghcr.io/actualbudget/actual:latest-alpine"
+        user: "${UID_GID}"
+        expose:
+          - "5006"
+        networks:
+          reverse_proxy:
+        volumes:
+          - /srv/actual-budget:/data:rw
+        healthcheck:
+          test: ["CMD", "node", "src/scripts/health-check.js"]
+          interval: 1m0s
+          timeout: 10s
+          retries: 3
+          start_period: 20s
+        restart: unless-stopped
+
+    networks:
+      reverse_proxy:
+        external: true
+  '';
 
   systemd.tmpfiles.rules = [
     "d /srv/actual-budget 0750 actual-budget actual-budget -"
