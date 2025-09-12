40778ca491e0f25682cb6ce832fbb3ea09ca7918
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-13 00:07:13 +0200

    [dashboard] inline homer.yml to dashboard.nix

--- modules/dashboard.nix
+++ modules/dashboard.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   users.users.homer = {
@@ -10,11 +11,32 @@
   };
   users.groups.homer.gid = 985;
 
-  dockerCompose.homer = {
+  dockerCompose.homer = let
+    uidGid = "${toString config.users.users.homer.uid}:${toString config.users.groups.homer.gid}";
+  in {
     enable = true;
-    composeFile = "${lib.flake-location}/compose/homer.yml";
-    environment = {
-      UID_GID = "${toString config.users.users.homer.uid}:${toString config.users.groups.homer.gid}";
-    };
+    composeFile = pkgs.writeText "homer.yml" ''
+      version: "3.8"
+      services:
+        dashboard:
+          image: b4bz/homer:latest
+          container_name: dashboard
+          expose:
+            - "8080"
+
+          networks:
+            reverse_proxy:
+
+          volumes:
+            - /etc/localtime:/etc/localtime:ro
+            - ${lib.flake-location}/dotfiles/homer:/www/assets:ro
+
+          user: "${uidGid}"
+          restart: unless-stopped
+
+      networks:
+        reverse_proxy:
+          external: true
+    '';
   };
 }
