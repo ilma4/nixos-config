466b2a0d426340cfd94a06bc21094a7632e4f234
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-12 22:19:56 +0200

    lidarr

--- hosts/laat/lidarr.nix
+++ hosts/laat/lidarr.nix
@@ -6,11 +6,11 @@
 }: {
   users.users.lidarr = {
     isSystemUser = true;
-    uid = 803;
+    uid = 804;
     group = config.users.groups.lidarr.name;
   };
   users.groups.lidarr = {
-    gid = 803;
+    gid = 804;
   };
 
   dockerCompose.lidarr = {
@@ -23,19 +23,56 @@
           ports:
             - "8686:8686"
           environment:
-            - PUID=${config.users.users.lidarr.uid}
-            - PGID=${config.users.users.lidarr.gid}
+            - PUID=${toString config.users.users.lidarr.uid}
+            - PGID=${toString config.users.groups.lidarr.gid}
             - UMASK=002
           volumes:
             - /srv/lidarr/config/lidarr:/config
-            - /srv/lidarr/data:/data
+            - /srv/lidarr/data/lidarr:/data
             - /etc/localtime:/etc/localtime:ro
+
+
+
+        # SABnzbd
+        SABnzbd:
+          image: ghcr.io/hotio/sabnzbd
+          volumes:
+            - /srv/lidarr/config/sabnzbd:/config
+            - /srv/lidarr/data/usenet:/data/usenet
+          environment:
+            - PUID=${toString config.users.users.lidarr.uid}
+            - PGID=${toString config.users.groups.lidarr.gid}
+            - UMASK=002
+          ports:
+            - "8088:8080"
+
+
+        # plex
+        Plex:
+          image: ghcr.io/hotio/plex
+          volumes:
+            - /srv/lidarr/config/plex:/config
+            - /srv/lidarr/data/media:/data/media
+
+          environment:
+            - PUID=${toString config.users.users.lidarr.uid}
+            - PGID=${toString config.users.groups.lidarr.gid}
+            - UMASK=002
     '';
   };
 
-  systemd.tmpfiles.rules = [
-    "d /srv/lidarr ${config.users.users.lidarr.uid} ${config.users.users.lidarr.gid} - -"
+  systemd.tmpfiles.rules = let
+    uid = toString config.users.users.lidarr.uid;
+    gid = toString config.users.groups.lidarr.gid;
+  in [
+    "d 0755  /srv/lidarr ${uid} ${gid} - -"
+    "d 0755  /srv/lidarr/data ${uid} ${gid} - -"
+    "d 0755  /srv/lidarr/data/torrents/music ${uid} ${gid} - -"
+    "d 0755  /srv/lidarr/data/usenet/music ${uid} ${gid} - -"
   ];
 
-  networking.firewall.allowedTCPPorts = [8686];
+  networking.firewall.allowedTCPPorts = [
+    8686
+    8088 # SABnzbd, TODO: remove
+  ];
 }
