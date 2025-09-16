9f89ee17c2e312ac8026c77ba0b780fda1f64392
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-16 10:28:55 +0200

    backup module

--- modules/backup.nix
+++ modules/backup.nix
@@ -8,6 +8,10 @@
     users.users.backup = {
       isSystemUser = true;
       group = config.users.groups.backup.name;
+      shell = pkgs.bash;
+      openssh.authorizedKeys.keys = [
+        "restrict,command=\"${pkgs.rclone}/bin/rclone serve restic --stdio --append-only /var/restic\",no-pty,no-agent-forwarding,no-port-forwarding,no-X11-forwarding ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGlG8sgxuAVYsgfcrJOXnoIVm8h/UYPPOCljmkpaiG+2 backup-key"
+      ];
     };
     users.groups.backup = {};
 
