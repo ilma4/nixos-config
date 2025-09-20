6d176a86c8befde70231b078fda5fbc64a9ba527
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-09-20 22:33:44 +0200

    fix

--- hosts/laat/immich.nix
+++ hosts/laat/immich.nix
@@ -18,6 +18,6 @@ in {
   # TODO: setup proper UID and GID for postgres, currently it runs as 999:999, but directory is mounted as root:root causing permission issues
   systemd.tmpfiles.rules = [
     "d /srv/immich/library 700 root root -"
-    "d /srv/immich/postgres 700 root root -"
+    "d /srv/immich/postgres 700 999 999 -"
   ];
 }
