852bdc5d1822ca2a908f1bcc5c12f7dbeeda2a16
Ilia Malakhov <ilya.malakhov4@gmail.com>
2025-11-01 23:02:34 +0100

    option to disable home-manager.nix

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,11 +1,21 @@
 {
+  config,
   inputs,
   pkgs-unstable,
+  lib,
   ...
 }: {
-  home-manager.useGlobalPkgs = true;
-  home-manager.extraSpecialArgs = {
-    inherit inputs;
-    inherit pkgs-unstable;
+  options.i4.home-manager.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "enable home-manager options";
+  };
+
+  config = lib.mkIf config.i4.home-manager.enable {
+    home-manager.useGlobalPkgs = true;
+    home-manager.extraSpecialArgs = {
+      inherit inputs;
+      inherit pkgs-unstable;
+    };
   };
 }
