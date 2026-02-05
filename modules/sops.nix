b4fd48e93b87016d53e1c33c40a4566a1c709c04
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-02-05 21:42:42 +0100

    cleanup

--- modules/sops.nix
+++ modules/sops.nix
@@ -18,13 +18,14 @@
   config = lib.mkIf config.i4.sops.enable {
     sops.defaultSopsFile = "${myLib.secrets}/example.yaml";
     sops.defaultSopsFormat = "yaml";
-    # sops.age.keyFile = "/home/ilma4/.config/sops/age/keys.txt";
-    # This will generate a new key if the key specified above does not exist
-    sops.age.generateKey = true;
+
     sops.age.keyFile =
       if (builtins.hasAttr "users" config) # true if NixOS/Nix-darwin
       then "/var/lib/sops-nix/age/keys.txt"
       else "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+
+    # This will generate a new key if the key specified above does not exist
+    sops.age.generateKey = true;
 
     # map i4-encrypted-files
     sops.secrets = lib.pipe config.i4-encrypted-files [
