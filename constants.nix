a80c2f7e9d1fad3d249c3e0fe4372b42b04d8608
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-02-07 19:01:40 +0100

    Add more constants

--- constants.nix
+++ constants.nix
@@ -1,5 +1,18 @@
 {
-  test-key = "asdfgasdgas";
-  some-numer = 1234;
   main-pub-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFdYWQA91YiviGcsXEVUf4/dbAU2So1AAa1qU6ZFlx7A";
+  github-pub-key = "TODO";
+
+  hetzer-restic = {
+    repo = "/home/restic";
+    password-file = "TODO";
+    old-password-file = null;
+  };
+
+  laat = {
+    restic = {
+      repo = "/mnt/hdd/restic";
+      password-file = "/run/secrets/..."; # TODO
+      old-password-file = null; # used to update password on repo
+    };
+  };
 }
