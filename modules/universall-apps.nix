c0a96151a3253da470c468b8ffa6caa5b8288784
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-04-06 14:57:44 +0200

    fix

--- modules/universall-apps.nix
+++ modules/universall-apps.nix
@@ -8,41 +8,30 @@
   inherit (lib) mkEnableOption mkIf mkMerge mkOption types;
   inherit (myLib.unifiedModules.checkers) isDarwin isHomeManager isLinux;
 
-  appType = types.submodule (name: {
+  appType = types.submodule {
     options = {
       enable = mkOption {
         type = types.bool;
         default = true;
       };
       linuxName = mkOption {
-        type = types.str;
-        default = name;
+        type = types.nullOr types.str;
+        default = null;
       };
       macName = mkOption {
-        type = types.str;
-        default = name;
+        type = types.nullOr types.str;
+        default = null;
       };
       linuxInstallation = mkOption {
-        type = types.nullOr (
-          types.enum [
-            "program"
-            "package"
-          ]
-        );
+        type = types.nullOr (types.enum ["program" "package"]);
         default = "program";
       };
       macInstallation = mkOption {
-        type = types.nullOr (
-          types.enum [
-            "cask"
-            "brew"
-            "package"
-          ]
-        );
+        type = types.nullOr (types.enum ["cask" "brew" "package"]);
         default = "cask";
       };
     };
-  });
+  };
 in {
   options.i4.apps = {
     apps = mkOption {
@@ -55,7 +44,24 @@ in {
 
   config = mkIf config.i4.apps.enable (
     let
-      enabledApps = lib.mapAttrsToList (_: app: app) (lib.filterAttrs (_: app: app.enable) config.i4.apps.apps);
+      appsFixedNames =
+        lib.mapAttrs (
+          name: app:
+            app
+            // (
+              if app.macName == null
+              then {macName = name;}
+              else {}
+            )
+            // (
+              if app.linuxName == null
+              then {linuxName = name;}
+              else {}
+            )
+        )
+        config.i4.apps.apps;
+
+      enabledApps = lib.mapAttrsToList (_: app: app) (lib.filterAttrs (_: app: app.enable) appsFixedNames);
 
       linuxProgramNames = map (app: app.linuxName) (lib.filter (app: app.linuxInstallation == "program") enabledApps);
 
@@ -65,12 +71,14 @@ in {
 
       macBrewNames = map (app: app.macName) (lib.filter (app: app.macInstallation == "brew") enabledApps);
 
-      macPackageNames = map (app: app.name) (lib.filter (app: app.macInstallation == "package") enabledApps);
+      macPackageNames = map (app: app.macName) (lib.filter (app: app.macInstallation == "package") enabledApps);
 
-      packageList =
+      packageNames =
         if isLinux
         then linuxPackageNames
         else macPackageNames;
+
+      packageList = map (name: pkgs.${name}) packageNames;
     in
       lib.optionalAttrs (!isHomeManager) {
         environment.systemPackages = packageList;
