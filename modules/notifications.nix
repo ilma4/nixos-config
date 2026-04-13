f1c676f3c205e005a8d44295bc9726e9e316e779
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-04-13 13:27:48 +0200

    setup apprise gateway instead of just cli

--- modules/notifications.nix
+++ modules/notifications.nix
@@ -8,6 +8,10 @@
   cfg = config.i4.notifications;
   telegramMyIdSecret = constants.telegram.my-id-secret;
   notificationsApiKeySecret = constants.telegram.notifications-api-key-secret;
+  appriseImage = "docker.io/caronc/apprise:latest";
+  appriseUidGid = "${toString config.users.users.apprise.uid}:${toString config.users.groups.apprise.gid}";
+  apprisePort = "18000";
+  appriseUrl = "http://127.0.0.1:${apprisePort}/notify/";
 in {
   options.i4.notifications = {
     enable = lib.mkEnableOption "notifications via apprise";
@@ -15,7 +19,7 @@ in {
     notify = lib.mkOption {
       type = lib.types.functionTo lib.types.str;
       readOnly = true;
-      description = "Generate a shell command that sends a notification using apprise.";
+      description = "Generate a shell command that sends a notification using the local apprise API.";
       example = lib.literalExpression ''config.i4.notifications.notify "Can't create backup"'';
       # default = _: throw "i4.notifications.enable must be true to use notify.";
     };
@@ -26,19 +30,57 @@ in {
       _module.args.notify = cfg.notify;
     }
     (lib.mkIf cfg.enable {
+      users.users.apprise = {
+        isSystemUser = true;
+        uid = 807;
+        group = "apprise";
+      };
+      users.groups.apprise.gid = config.users.users.apprise.uid;
+
       sops.secrets.${telegramMyIdSecret} = {};
       sops.secrets.${notificationsApiKeySecret} = {};
 
-      sops.templates."apprise.conf" = {
+      sops.templates."apprise.env" = {
         content = ''
-          tgram://bot${config.sops.placeholder.${notificationsApiKeySecret}}/${config.sops.placeholder.${telegramMyIdSecret}}/
+          APPRISE_STATELESS_URLS=tgram://bot${config.sops.placeholder.${notificationsApiKeySecret}}/${config.sops.placeholder.${telegramMyIdSecret}}/
         '';
         mode = "0400";
         owner = "root";
         group = "root";
+        restartUnits = ["apprise.service"];
       };
 
-      i4.notifications.notify = message: "${lib.getExe pkgs.apprise} --config ${lib.escapeShellArg config.sops.templates."apprise.conf".path} --body ${lib.escapeShellArg message}";
+      dockerCompose.apprise = {
+        composeText = ''
+          name: apprise
+
+          services:
+            apprise:
+              image: ${appriseImage}
+              container_name: apprise
+              restart: unless-stopped
+              read_only: true
+              user: "${appriseUidGid}"
+              ports:
+                - "127.0.0.1:${apprisePort}:8000"
+              env_file:
+                - ${config.sops.templates."apprise.env".path}
+              environment:
+                APPRISE_STATEFUL_MODE: "disabled"
+                ALLOWED_HOSTS: "127.0.0.1 localhost"
+                TZ: "${config.time.timeZone}"
+              tmpfs:
+                - /tmp
+              cap_drop:
+                - ALL
+              security_opt:
+                - no-new-privileges:true
+        '';
+      };
+
+      i4.notifications.notify = message: let
+        payload = builtins.toJSON {body = message;};
+      in "${lib.getExe pkgs.curl} --fail --silent --show-error --header 'Content-Type: application/json' --data ${lib.escapeShellArg payload} ${lib.escapeShellArg appriseUrl}";
     })
   ];
 }
