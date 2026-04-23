2e728ac91631b6600306758aa81cf09a1b7061b3
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-04-23 18:54:41 +0200

    update home-assistant 2026.1.3 -> 2026.4.3

--- hosts/nas/docker-services/home-assistant.nix
+++ hosts/nas/docker-services/home-assistant.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
   CONFIG_DIR = "/srv/homeassistant";
-  home-assistant-version = "2026.1.3";
+  home-assistant-version = "2026.4.3";
 in {
   users.users.homeassistant = {
     isSystemUser = true;
