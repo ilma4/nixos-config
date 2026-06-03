01dc1c7c2669500144be5eb722b73d387f903bb3
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-06-03 20:28:03 +0200

    remove unnecessary overlay

--- overlays/darwin-signing-workaround-overlay.nix
+++ overlays/darwin-signing-workaround-overlay.nix
@@ -1,11 +1,2 @@
 final: prev: {
-  fish = prev.fish.overrideAttrs (_old: {
-    # Bust the cache key to force a local rebuild, ensuring valid codesigning
-    # on Apple Silicon. See: https://github.com/NixOS/nixpkgs/issues/507531
-    NIX_FORCE_LOCAL_REBUILD = "darwin-codesign-fix";
-  });
-  ffmpeg = prev.ffmpeg.overrideAttrs (_old: {
-    # Bust the cache key to force a local rebuild, ensuring valid codesigning
-    NIX_FORCE_LOCAL_REBUILD = "darwin-codesign-fix";
-  });
 }
