3e530efd744d5fa0bd2af3e0aa316d6d5fc22bda
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-08-08 10:02:52 +0200

    [zed] install lsp servers via nixpkgs

--- home/zed.nix
+++ home/zed.nix
@@ -6,14 +6,41 @@
 }: let
   cfg = config.i4.zed;
   isDarwin = pkgs.stdenv.isDarwin;
+  languageServers = with pkgs; [
+    bash-language-server
+    basedpyright
+    docker-language-server
+    haskell-language-server
+    # kotlin-lsp # TODO uncomment when added to nixpkgs
+    # kotlin-language-server # do not add, obsolete
+    lemminx
+    markdown-oxide
+    nil
+    nixd
+    package-version-server
+    ruff
+    rust-analyzer
+    sourcekit-lsp
+    taplo
+    texlab
+    vscode-langservers-extracted
+    vtsls
+    yaml-language-server
+  ];
 in {
   options.i4.zed = {
     enable = lib.mkEnableOption "Zed editor configuration";
   };
 
   config = lib.mkIf cfg.enable {
+    # Zed prefers language servers found in PATH over downloading its own.
+    # The Linux package is wrapped with this PATH; Homebrew Zed on Darwin
+    # discovers the same binaries through the Home Manager profile.
+    home.packages = lib.optionals isDarwin languageServers;
+
     programs.zed-editor = {
       enable = !isDarwin;
+      extraPackages = languageServers;
     };
   };
 }
