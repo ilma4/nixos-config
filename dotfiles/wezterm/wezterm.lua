6e0a77df46694c3309de881e5ce1bc192b05f895
Ilia Malakhov <ilya.malakhov4@gmail.com>
2026-08-18 20:32:28 +0200

    [wezterm] don't use ligatures and header follows colorscheme

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -12,8 +12,19 @@ local function color_scheme_for_appearance(appearance)
 end
 
 config.font = wezterm.font("JetBrains Mono")
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 13.0
 config.color_scheme = color_scheme_for_appearance(wezterm.gui.get_appearance())
+
+local colors = wezterm.color.get_builtin_schemes()[config.color_scheme]
+config.window_frame = {
+  active_titlebar_bg = colors.background,
+  inactive_titlebar_bg = colors.background,
+  active_titlebar_fg = colors.foreground,
+  inactive_titlebar_fg = colors.foreground,
+  button_bg = colors.background,
+  button_fg = colors.foreground,
+}
 
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE' -- use header, as tabbar
 config.window_close_confirmation = 'NeverPrompt'
